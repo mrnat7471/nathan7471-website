e21d0f6b70eab9846b820482750b1e1c150014ef
nathan <mrnat7471@gmail.com>
2024-08-18 18:07:51 +0100

    New CV Site v2

--- README.md
+++ README.md
@@ -1,40 +1,47 @@
-This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
+<div align="center">
+<img alt="Portfolio" src="https://github.com/dillionverma/portfolio/assets/16860528/57ffca81-3f0a-4425-b31d-094f61725455" width="90%">
+</div>
 
-## Getting Started
+# Portfolio [![Deploy with Vercel](https://vercel.com/button)](https://vercel.com/new/clone?repository-url=https%3A%2F%2Fgithub.com%2Fdillionverma%2Fportfolio)
 
-First, run the development server:
+Built with next.js, [shadcn/ui](https://ui.shadcn.com/), and [magic ui](https://magicui.design/), deployed on Vercel.
 
-```bash
-npm run dev
-# or
-yarn dev
-# or
-pnpm dev
-# or
-bun dev
-```
+# Features
 
-Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
+- Setup only takes a few minutes by editing the [single config file](./src/data/resume.tsx)
+- Built using Next.js 14, React, Typescript, Shadcn/UI, TailwindCSS, Framer Motion, Magic UI
+- Includes a blog
+- Responsive for different devices
+- Optimized for Next.js and Vercel
 
-You can start editing the page by modifying `pages/index.tsx`. The page auto-updates as you edit the file.
+# Getting Started Locally
 
-[API routes](https://nextjs.org/docs/api-routes/introduction) can be accessed on [http://localhost:3000/api/hello](http://localhost:3000/api/hello). This endpoint can be edited in `pages/api/hello.ts`.
+1. Clone this repository to your local machine:
 
-The `pages/api` directory is mapped to `/api/*`. Files in this directory are treated as [API routes](https://nextjs.org/docs/api-routes/introduction) instead of React pages.
+   ```bash
+   git clone https://github.com/dillionverma/portfolio
+   ```
 
-This project uses [`next/font`](https://nextjs.org/docs/basic-features/font-optimization) to automatically optimize and load Inter, a custom Google Font.
+2. Move to the cloned directory
 
-## Learn More
+   ```bash
+   cd portfolio
+   ```
 
-To learn more about Next.js, take a look at the following resources:
+3. Install dependencies:
 
-- [Next.js Documentation](https://nextjs.org/docs) - learn about Next.js features and API.
-- [Learn Next.js](https://nextjs.org/learn) - an interactive Next.js tutorial.
+   ```bash
+   pnpm install
+   ```
 
-You can check out [the Next.js GitHub repository](https://github.com/vercel/next.js/) - your feedback and contributions are welcome!
+4. Start the local Server:
 
-## Deploy on Vercel
+   ```bash
+   pnpm dev
+   ```
 
-The easiest way to deploy your Next.js app is to use the [Vercel Platform](https://vercel.com/new?utm_medium=default-template&filter=next.js&utm_source=create-next-app&utm_campaign=create-next-app-readme) from the creators of Next.js.
+5. Open the [Config file](./src/data/resume.tsx) and make changes
 
-Check out our [Next.js deployment documentation](https://nextjs.org/docs/deployment) for more details.
+# License
+
+Licensed under the [MIT license](https://github.com/dillionverma/portfolio/blob/main/LICENSE.md).
---
+++ src/data/resume.tsx
@@ -0,0 +1,285 @@
+import { Icons } from "@/components/icons";
+import { HomeIcon, NotebookIcon } from "lucide-react";
+
+function calculateAge(birthday: string): number {
+  // Convert the birthday string into a Date object
+  const birthDate = new Date(birthday);
+
+  // Get today's date
+  const today = new Date();
+
+  // Calculate the age
+  let age = today.getFullYear() - birthDate.getFullYear();
+
+  // Adjust the age if the birthday hasn't occurred yet this year
+  const monthDifference = today.getMonth() - birthDate.getMonth();
+  const dayDifference = today.getDate() - birthDate.getDate();
+
+  if (monthDifference < 0 || (monthDifference === 0 && dayDifference < 0)) {
+    age--;
+  }
+
+  return age;
+}
+
+export const DATA = {
+  name: "Nathan Powell",
+  location: "Manchester, United Kingdom",
+  initials: "NP",
+  url: "https://nathanpowell.uk",
+  description:
+    `A passionate ${calculateAge('2002-07-07')}-year-old Full Stack Software Engineer skilled in JavaScript, TypeScript, Python, React, and Node.js, driven by a commitment to innovation and creating impactful web solutions.`,
+  summary:
+    `Hello Humans 👋\n\n As a passionate Full Stack Software Engineer, I bring a dynamic blend of cutting-edge technical skills and a fresh, creative approach to software development. At just ${calculateAge('2002-07-07')} years old, I have already made significant strides in both front-end and back-end technologies. My journey in coding began early, allowing me to develop a deep understanding of languages such as JavaScript, TypeScript, and Python, along with frameworks like React and Node.js.\n\nMy experience includes building responsive and user-friendly web applications, diving deep into databases, and crafting efficient server-side algorithms. What sets me apart is my eagerness to embrace new challenges and my commitment to staying on top of industry trends. My coding is not just a profession; it’s a passion that drives me to create innovative solutions that resonate with users.`,
+  avatarUrl: "/avatar.png",
+  skills: [
+      "Python",
+      "Javascript",
+      "Typescript",
+      "Java",
+      "C++",
+      "Rust",
+      "CSS",
+      "HTML",
+      "Vue.js",
+      "Django",
+      "FastAPI",
+      "React",
+      "NuxtJS",
+      "NextJS",
+      "Electron",
+      "Node.js",
+      "Express",
+      "TailwindCSS",
+      "Bootstrap",
+      "Material-UI",
+      "MySQL",
+      "PostgreSQL",
+      "SQLite",
+      "Tauri"
+
+  ],
+  navbar: [
+    { href: "/", icon: HomeIcon, label: "Home" },
+    { href: "/Nathan-Powell-CV.pdf", icon: NotebookIcon, label: "PDF CV" },
+  ],
+  contact: {
+    email: "nathan@omnibyte.tech",
+    tel: "+441615240093",
+    social: {
+      GitHub: {
+        name: "GitHub",
+        url: "https://github.com/mrnat7471",
+        icon: Icons.github,
+
+        navbar: true,
+      },
+      LinkedIn: {
+        name: "LinkedIn",
+        url: "https://www.linkedin.com/in/nathan-t-powell/",
+        icon: Icons.linkedin,
+
+        navbar: true,
+      },
+      X: {
+        name: "X",
+        url: "https://x.com/Natpow7471",
+        icon: Icons.x,
+
+        navbar: true,
+      },
+      Youtube: {
+        name: "Youtube",
+        url: "https://dub.sh/dillion-youtube",
+        icon: Icons.youtube,
+        navbar: false,
+      },
+      email: {
+        name: "Email",
+        url: "mailto:contact@omnibyte.tech",
+        icon: Icons.email,
+
+        navbar: true,
+      },
+    },
+  },
+
+  work: [
+    {
+      company: "Omnibyte Technologies",
+      href: "https://omnibyte.tech",
+      badges: [],
+      location: "Remote",
+      title: "Director",
+      logoUrl: "/omnibyte.jpeg",
+      start: "Feb 2022",
+      end: "Present",
+      description:
+        "One of the co-founder of Omnibyte Technologies. We specialize in bespoke software development, web development, and project maintenance. Our work includes projects like the \"Drivers Hub\" and a custom Discord bot for Prime Logistics, as well as TrackSim, a job logging solution for simulation games. I handle all aspects of day-to-day company management, ensuring we deliver high-quality technology solutions to our clients around the globe.",
+    },
+    {
+      company: "Roco Technology Group",
+      href: "https://rocotg.co.uk",
+      badges: [],
+      location: "Hybrid",
+      title: "Software Engineer",
+      logoUrl: "/roco.jpeg",
+      start: "Jun 2021",
+      end: "Present",
+      description: "",
+    },
+    {
+      company: "Nathan7471 Development",
+      href: "https://nathanpowell.uk",
+      badges: [],
+      location: "Remote",
+      title: "Freelance Web Developer",
+      logoUrl: "/nathan7471.png",
+      start: "Jun 2020",
+      end: "Feb 2022",
+      description: "",
+    }
+  ],
+  education: [
+    {
+      school: "BPP University",
+      href: "https://bpp.com",
+      degree: "BSc (Hons), Digital and Technology Solutions",
+      logoUrl: "/bpp.jpeg",
+      start: "2023",
+      end: "2025",
+    },
+    {
+      school: "Estio Training",
+      href: "https://estio.co.uk",
+      degree: "BSc Level 4, Software Developer",
+      logoUrl: "/estio.jpeg",
+      start: "2021",
+      end: "2023",
+    },
+    {
+      school: "Calderdale College",
+      href: "https://www.calderdale.ac.uk/",
+      degree: "BTEC Level 3 Extended Diploma, Computing",
+      logoUrl: "/calderdale.png",
+      start: "2019",
+      end: "2021",
+    },
+  ],
+  projects: [
+    {
+      title: "VTC Gateway",
+      associated: 'Omnibyte Technologies',
+      href: "https://vtcgateway.com",
+      dates: "Dec 2023 - Present",
+      active: true,
+      description:
+        "Welcome to VTC Gateway, your ultimate online hub for virtual trucking adventures! Dive into the open road with friends or virtual trucking companies, where you can enjoy robust customization, user-friendly features, and a thriving community. Create or join a virtual trucking company and embark on epic journeys tailored to your preferences. At VTC Gateway, we put you in control—experience trucking like never before. Visit our website today and start your journey!",
+      technologies: [
+          "Next.js",
+          "Typescript",
+          "MySQL",
+          "TailwindCSS",
+          "Stripe",
+          "NextUI",
+          "Python",
+          "Django",
+          "Django Rest Framework"
+      ],
+      links: [
+        {
+          type: "Website",
+          href: "https://vtcgateway.com",
+          icon: <Icons.globe className="size-3" />,
+        },
+      ],
+      image: "https://i.imgur.com/dNPjMey.png",
+      video: "",
+    },
+    {
+      title: "Simulator Hits Website & Panel",
+      associated: null,
+      href: "https://simulatorhits.com",
+      dates: "Jun 2023 - Present",
+      active: true,
+      description:
+          "I have been providing support and training on a consultancy level as they have migrated their site to a more modern framework suitable for their station. We have now built an API and Basic Panel for the station with more features to come.",
+      technologies: [
+        "Next.js",
+        "Typescript",
+        "MySQL",
+        "TailwindCSS",
+        "Python",
+        "Django",
+        "Django Rest Framework"
+      ],
+      links: [
+        {
+          type: "Website",
+          href: "https://simulatorhits.com",
+          icon: <Icons.globe className="size-3" />,
+        },
+      ],
+      image: "https://i.imgur.com/SWaYNbB.png",
+      video: "",
+    },
+    {
+      title: "TrackSim",
+      associated: 'Omnibyte Technologies',
+      href: "https://simulatorhits.com",
+      dates: "Jan 2023 - Present",
+      active: true,
+      description:
+          "A job logging solution for American Truck Simulator and Euro Truck Simulator 2 that processes the game data and sends it straight to your Drivers Hub with no fuss!\n\nWe know it’s not easy to create your own tracker. That’s why we have made this solution so you don’t have to worry about creating one.\n\nWith TrackSim, we make it simple. Your drivers install our client, you setup the integration to your Drivers Hub and the jobs your drivers log go straight through to your Drivers Hub, no fuss.",
+      technologies: [
+        "Next.js",
+        "Typescript",
+        "MySQL",
+        "Stripe",
+        "TailwindCSS",
+        "Python",
+        "Django",
+        "Django Rest Framework",
+        "C++",
+        "Java",
+      ],
+      links: [
+        {
+          type: "Website",
+          href: "https://tracksim.app",
+          icon: <Icons.globe className="size-3" />,
+        },
+      ],
+      image: "",
+      video: "/tracksim.mp4",
+    },
+    {
+      title: "Prime Logistics Drivers Hub",
+      associated: 'Omnibyte Technologies',
+      href: "https://primevtc.com",
+      dates: "Jun 2021 - Present",
+      active: true,
+      description:
+          "Managing and maintaining a panel for 800+ gamers and tracking and processing 1000s of events per day whilst developing new features on the backend and frontend.Managing and maintaining a panel for 800+ gamers and tracking and processing 1000s of events per day whilst developing new features on the backend and frontend.",
+      technologies: [
+        "Next.js",
+        "Typescript",
+        "MySQL",
+        "Stripe",
+        "TailwindCSS",
+        "Python",
+        "Django",
+        "Django Rest Framework",
+      ],
+      links: [
+        {
+          type: "Website",
+          href: "https://primevtc.com",
+          icon: <Icons.globe className="size-3" />,
+        },
+      ],
+      image: "https://i.imgur.com/DDabFuY.png",
+      video: "",
+    }
+  ],
+} as const;
